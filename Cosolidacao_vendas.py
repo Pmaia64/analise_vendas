f268dd0dd161451220693a4a2fa7defcb2d478d2
Paulo Maia <pmaia64@hotmail.com>
2023-05-18 20:30:47 -0400

    Primeiro commit Importando biblioteca

--- Cosolidacao_vendas.py
+++ Cosolidacao_vendas.py
@@ -1,11 +1,12 @@
 # Importar biblioteca os ( para percorrer pastas de arquivos - base de dados)
 # Leitura de arquivos em uma pasta
-# Utilização do for (importar a base de dados)
+# Utilização do for (executa a base de dados para cada arquivo)
 
 
 import os
-lista_arquivo = os.listdir('C:/Users/Paulo Maia/Desktop/Projeto 4 Consolidação Vendas/Vendas_Lojas')
+lista_arquivo = os.listdir("C:/Users/Paulo Maia/Desktop/Projeto 4 Consolidação Vendas/Vendas")
 print(lista_arquivo)
 for arquivo in lista_arquivo:
     print(arquivo)
-    
+
+
---
+++ Readme.md
@@ -0,0 +1,8 @@
+Projeto para consolidação de vendas de uma cadeia de lojas
+Percorrer todos os arquivos de uma base de dados;
+Importar as bases de dados de vendas;
+Tratar/compilar as bases de dados
+Calcular o produto mais vendido
+Calcular o produto que mais faturou
+Calcular a loja que mais vendeu (em faturamento)
+Criar um gráfico demonstrativo 
