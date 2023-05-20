d5576638801eddcf4e14f6dc4d7137a12444deb4
Paulo Maia <pmaia64@hotmail.com>
2023-05-19 21:24:47 -0400

    Importação de dadps de vendas

--- Cosolidacao_vendas.py
+++ Cosolidacao_vendas.py
@@ -4,9 +4,16 @@
 
 
 import os
+import pandas as pd
 lista_arquivo = os.listdir("C:/Users/Paulo Maia/Desktop/Projeto 4 Consolidação Vendas/Vendas")
 
+tabela_total = pd.DataFrame()
+#Importar as bases de dados de vendas
 for arquivo in lista_arquivo:
     if "Vendas" in arquivo:
-        print(f"C:/Users/Paulo Maia/Desktop/Projeto 4 Consolidação Vendas/Vendas/{arquivo}")
-
+        
+        #importar o arquivo
+        tabela = pd.read_csv(f"C:/Users/Paulo Maia/Desktop/Projeto 4 Consolidação Vendas/Vendas/{arquivo}")
+        tabela_total = tabela_total.append(tabela)
+        
+print(tabela_total)
