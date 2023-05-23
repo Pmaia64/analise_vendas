90810a92b6a6e482d9c5119b958aefebda4f2a3b
Paulo Maia <pmaia64@hotmail.com>
2023-05-22 22:44:22 -0400

    codigo para analise de dados

--- Cosolidacao_vendas.py
+++ Cosolidacao_vendas.py
@@ -2,9 +2,11 @@
 # Leitura de arquivos em uma pasta
 # Utilização do for (executa a base de dados para cada arquivo)
 
-
 import os
 import pandas as pd
+import plotly.express as px
+
+
 lista_arquivo = os.listdir("C:/Users/Paulo Maia/Desktop/Projeto 4 Consolidação Vendas/Vendas")
 
 tabela_total = pd.DataFrame()
@@ -12,8 +14,22 @@ tabela_total = pd.DataFrame()
 for arquivo in lista_arquivo:
     if "Vendas" in arquivo:
         
-        #importar o arquivo
         tabela = pd.read_csv(f"C:/Users/Paulo Maia/Desktop/Projeto 4 Consolidação Vendas/Vendas/{arquivo}")
         tabela_total = tabela_total.append(tabela)
-        
 print(tabela_total)
+
+tabela_produtos = tabela_total.groupby('Produto').sum()
+tabela_produtos = tabela_produtos[['Quantidade Vendida', 'Preco Unitario']].sort_values(by='Quantidade Vendida', ascending=False)
+print(tabela_produtos)
+
+tabela_total['Faturamento'] = tabela_total['Quantidade Vendida'] * tabela_total['Preco Unitario']
+tabela_faturamento = tabela_total.groupby('Produto').sum()
+tabela_faturamento = tabela_faturamento[['Faturamento']].sort_values(by='Faturamento', ascending=False)
+print(tabela_faturamento)
+
+tabela_lojas = tabela_total.groupby('Loja').sum()
+tabela_lojas = tabela_lojas[['Faturamento']].sort_values(by='Faturamento', ascending=False)
+print(tabela_lojas)
+
+grafico = px.bar(tabela_lojas, x=tabela_lojas.index, y='Faturamento')
+grafico.show()
